f7fb5cb39814cf49b5d1191fb7d69bc3cffd7914
Rodrique09 <ddtxrodrique@gmail.com>
2025-10-21 00:29:43 -0500

    Adding responses using useEffect to the Chatbot

--- chatbot-project/src/App.jsx
+++ chatbot-project/src/App.jsx
@@ -1,10 +1,20 @@
-import { useState} from 'react'
+import { useState, useEffect} from 'react'
 import { ChatInput } from './components/ChatInput'
 import ChatMessages from './components/ChatMessages'
 import './App.css'
+import { Chatbot } from 'supersimpledev';
 
 function App(){
         const [chatMessages, setChatMessages] = useState([]);
+
+        useEffect(()=>{
+          Chatbot.addResponses({
+            'goodbye':'Goodbye. have a great day',
+            'give me a unique id': function() {
+              return `Sure! Here is a unique id:${crypto.randomUUID()}`; 
+            }
+          });
+        }, []);
 
         return (
           <div className = "app-container">
