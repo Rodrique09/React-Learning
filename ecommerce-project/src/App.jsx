4e6dc5e759ded395be157e86f13387df1b50d815
Rodrique09 <ddtxrodrique@gmail.com>
2025-10-25 10:36:13 -0500

    Crete multiple pages using react-roter

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -1,3 +1,4 @@
+import {Routes , Route} from 'react-router'
 import { HomePage } from './pages/Homepage'
 import './App.css'
 import './index.css'
@@ -5,7 +6,10 @@ import './index.css'
 const App = () =>{
 
   return (
-   <HomePage />
+    <Routes>
+      <Route path = "/" element = {<HomePage />} />
+      <Route path = "checkout" element =  {<div>This is a test checkout element</div>} />
+    </Routes>
   )
 }
 
