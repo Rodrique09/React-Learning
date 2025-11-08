7b0dfd25593d193dcd1ecb7b7209a87637d32459
Rodrique09 <ddtxrodrique@gmail.com>
2025-11-08 10:09:03 -0600

    Used beforeEach() to remove duplication

--- ecommerce-project/src/pages/home/Product.test.jsx
+++ ecommerce-project/src/pages/home/Product.test.jsx
@@ -1,5 +1,5 @@
 import axios from 'axios'
-import { it, expect, describe, vi } from 'vitest'
+import { it, expect, describe, vi, beforeEach } from 'vitest'
 import { Product } from './Product'
 import { render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event';
@@ -8,8 +8,11 @@ vi.mock('axios');
 
 
 describe('Product component', () => {
-    it('displays the product details correctly', () => {
-        const product = {
+    let product;
+    let loadCart;
+
+    beforeEach(() => {
+        product = {
             id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
             image: "images/products/athletic-cotton-socks-6-pairs.jpg",
             name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
@@ -20,11 +23,12 @@ describe('Product component', () => {
             priceCents: 1090,
             keywords: ["socks", "sports", "apparel"]
         };
+        loadCart = vi.fn()
+    });
 
-        const loadCart = vi.fn();
-
+    it('displays the product details correctly', () => {
         render(<Product product={product} loadCart={loadCart} />);
-        
+
         expect(
             screen.getByText('Black and Gray Athletic Cotton Socks - 6 Pairs')
         ).toBeInTheDocument();
@@ -35,36 +39,22 @@ describe('Product component', () => {
 
         expect(
             screen.getByTestId('product-image')
-        ).toHaveAttribute('src','/src/assets/images/products/athletic-cotton-socks-6-pairs.jpg');
+        ).toHaveAttribute('src', '/src/assets/images/products/athletic-cotton-socks-6-pairs.jpg');
 
         expect(
             screen.getByTestId('product-rating-stars')
-        ).toHaveAttribute('src','/src/assets/images/ratings/rating-45.png');
+        ).toHaveAttribute('src', '/src/assets/images/ratings/rating-45.png');
 
         expect(
             screen.getByText('87')
         ).toBeInTheDocument();
     });
 
-    it('add a product to the cart', async() => {
-        const product = {
-            id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-            image: "images/products/athletic-cotton-socks-6-pairs.jpg",
-            name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
-            rating: {
-                stars: 4.5,
-                count: 87
-            },
-            priceCents: 1090,
-            keywords: ["socks", "sports", "apparel"]
-        };
-        const loadCart = vi.fn();
+    it('add a product to the cart', async () => {
         render(<Product product={product} loadCart={loadCart} />);
-
         const user = userEvent.setup();
         const addToCartButton = screen.getByTestId('add-to-cart-button');
         await user.click(addToCartButton);
-
         expect(axios.post).toHaveBeenCalledWith(
             '/api/cart-items',
             {
